1a0bf299d30eef77357201900437340132d6e253
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-19 11:05:12 -0500

    añadir variables ciudad, enemigo y console.log de respuesta

--- nivel_1_variables/heroe1.js
+++ nivel_1_variables/heroe1.js
@@ -1,3 +1,6 @@
 const heroe1 = "Kevin";
 const poder = "hielo";
 let nivelEnergia = 100;
+const ciudad = "Trujillo";
+const enemigo = "El cuy magico";
+console.log("Mi nombre es " + heroe1 + " mi poder es el de " + poder + ", mi energia es de " + nivelEnergia + ", mi ciudad natal es " + ciudad + " y mi enemigo es " + enemigo);
