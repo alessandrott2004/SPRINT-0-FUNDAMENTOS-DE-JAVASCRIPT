3b42f0e58016ea5f626ffbb9d7427e6d58048294
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-19 11:43:31 -0500

    funcion aumentarFuerza

--- nivel_3_objetos/minirpg2.js
+++ nivel_3_objetos/minirpg2.js
@@ -8,3 +8,7 @@ function aumentarVida(jugador1){
 jugador1.vida +=30;
 console.log("la vida de " + jugador1.nombre + "ha aumentado a " + jugador1.vida);
 }
+function aumentarFuerza(jugador1){
+jugador1.fuerza +=20;
+console.log("la fuerza de " + jugador1.nombre + " a aumentado a " + jugador1.fuerza);
+}
