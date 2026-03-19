329508a7e9276d9c6807375f80da553d418a11b2
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-19 11:22:12 -0500

    funcion pagar

--- nivel_3_objetos/restaurante2.js
+++ nivel_3_objetos/restaurante2.js
@@ -7,3 +7,7 @@ function ordenarComida(cliente){
 cliente.hambre -= 30;
 console.log("tu hambre ha sido saciada, ahora tienes " + cliente.hambre + " de hambre");
 };
+function pagar(cliente){
+cliente.dinero -=10;
+console.log("has pagado por la comida, ahora tienes "+ cliente.dinero + " de dinero");
+};
