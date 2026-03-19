355645cbd79792524d6f02385de024c2d20cde60
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-19 11:43:12 -0500

    funcion aumentarVida

--- nivel_3_objetos/minirpg2.js
+++ nivel_3_objetos/minirpg2.js
@@ -1,5 +1,10 @@
 let jugador1 ={
-nombre:Alessandro,
+nombre:"Alessandro",
 vida:100,
 fuerza:40,
 nivel:20,
+}
+function aumentarVida(jugador1){
+jugador1.vida +=30;
+console.log("la vida de " + jugador1.nombre + "ha aumentado a " + jugador1.vida);
+}
