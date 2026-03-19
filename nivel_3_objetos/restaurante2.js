ac8c2d0ac1ad667b87376458e1fec3b4c01a0515
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-19 11:22:41 -0500

    funcion mostrarEstado

--- nivel_3_objetos/restaurante2.js
+++ nivel_3_objetos/restaurante2.js
@@ -11,3 +11,5 @@ function pagar(cliente){
 cliente.dinero -=10;
 console.log("has pagado por la comida, ahora tienes "+ cliente.dinero + " de dinero");
 };
+function mostrarEstado(cliente){
+console.log("El cliente " + cliente.nombre + " tiene " + cliente.hambre + " de hambre y tiene " + cliente.dinero + " de dinero");
