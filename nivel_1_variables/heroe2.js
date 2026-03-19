44813f64c125021018d968c42608c410adef2964
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-19 11:09:03 -0500

    modificar nivelEnergia y console.log con la respuesta

--- nivel_1_variables/heroe2.js
+++ nivel_1_variables/heroe2.js
@@ -4,3 +4,7 @@ let nivelEnergia = 100;
 let ciudad = "Lima"; 
 let enemigo = "Zombie"; 
 console.log("Mi nombre es " + heroe1 + " mi poder es el de " + poder + ", mi energia es de " + nivelEnergia + ", mi ciudad natal es " + ciudad + " y mi enemigo es " + enemigo);
+nivelEnergia = 1;
+console.log(nombreHeroe +  "ha recibido un daño critico de parte de" + enemigo );
+console.log("ahora su nivel de energia es de "+ nivelEnergia);
+
