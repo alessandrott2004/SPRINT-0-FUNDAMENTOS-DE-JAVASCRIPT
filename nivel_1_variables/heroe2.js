4b3d5fd35a03c7aad961af3c5327fb3f9de9e15a
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-19 11:07:40 -0500

    console.log de respuesta

--- nivel_1_variables/heroe2.js
+++ nivel_1_variables/heroe2.js
@@ -3,3 +3,4 @@ let poder = "electricidad";
 let nivelEnergia = 100; 
 let ciudad = "Lima"; 
 let enemigo = "Zombie"; 
+console.log("Mi nombre es " + heroe1 + " mi poder es el de " + poder + ", mi energia es de " + nivelEnergia + ", mi ciudad natal es " + ciudad + " y mi enemigo es " + enemigo);
