8d20df06ea4769347c3019e6e8226bfb92767d49
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-19 11:20:42 -0500

    funcion ordenarComida

--- nivel_3_objetos/restaurante2.js
+++ nivel_3_objetos/restaurante2.js
@@ -2,3 +2,8 @@ let cliente = {
 nombre: "Alessandro",
 dinero: 100,
 hambre:100
+};
+function ordenarComida(cliente){
+cliente.hambre -= 30;
+console.log("tu hambre ha sido saciada, ahora tienes " + cliente.hambre + " de hambre");
+};
