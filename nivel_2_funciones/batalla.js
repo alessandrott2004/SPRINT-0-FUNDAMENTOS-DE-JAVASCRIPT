c8679c51a9732e8b1d682540113dc526e3018c57
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-19 11:10:45 -0500

    funcion ataqueKevin

--- nivel_2_funciones/batalla.js
+++ nivel_2_funciones/batalla.js
@@ -1,2 +1,6 @@
 let energiakevin = 100;
 let energiaAlessandro = 100;
+function ataqueKevin() {
+    energiaAlessandro -= 20;
+    console.log("Kevin ataca. Energía de Alessandro: " + energiaAlessandro);
+}
