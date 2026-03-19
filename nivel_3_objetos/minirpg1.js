5f8ba6a384086e74eabd7bf0c8f5eea8264b166f
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-19 11:32:38 -0500

    funcion  recibirDaño

--- nivel_3_objetos/minirpg1.js
+++ nivel_3_objetos/minirpg1.js
@@ -3,3 +3,13 @@ let jugador = {
     vida: 150,
     fuerza: 10,
     nivel: 2
+        
+function subirNivel(jugador) {
+    jugador.nivel += 1;
+    jugador.fuerza += 20;
+    console.log("¡NIVEL UP! " + jugador.nombre + " ahora es nivel " + jugador.nivel);
+}
+function recibirDano(jugador) {
+    jugador.vida -= 15;
+    console.log(jugador.nombre + " recibió daño. Vida: " + jugador.vida);
+}
