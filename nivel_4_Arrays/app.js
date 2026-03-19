95ec118e2b40285e466377c4952b62cac69b2604
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-19 12:06:15 -0500

    añadir mas platos al array

--- nivel_4_Arrays/app.js
+++ nivel_4_Arrays/app.js
@@ -2,7 +2,9 @@
 let menu = [
  { nombre: "Arroz con pollo", precio: 12, stock: 5 },
  { nombre: "Lomo saltado", precio: 18, stock: 3 },
- { nombre: "Sopa", precio: 8, stock: 10 }
+ { nombre: "Sopa", precio: 8, stock: 10 },
+ { nombre: "chaufa", precio: 11, stock: 7 },
+ { nombre: "causa rellena", precio: 20, stock: 4 }
 ];
 
 // 2) FUNCIÓN: renderizar (mostrar) el menú en pantalla
