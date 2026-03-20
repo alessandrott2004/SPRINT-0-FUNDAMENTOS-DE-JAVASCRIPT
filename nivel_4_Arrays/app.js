050b41450e81a90768d3b8745468164e1eb15248
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-20 13:20:56 -0500

    algunos cambios y correciones

--- nivel_4_Arrays/app.js
+++ nivel_4_Arrays/app.js
@@ -1,10 +1,10 @@
 // 1) VARIABLES + OBJETOS + ARRAYS
 let menu = [
-  { nombre: "Arroz con pollo", precio: 12, stock: 5 },
-  { nombre: "Lomo saltado", precio: 18, stock: 9 },
-  { nombre: "Sopa", precio: 8, stock: 10 },
-  { nombre: "Causa rellena", precio: 20, stock: 5 },
-  { nombre: "Cuy chactado", precio: 30, stock: 7 }
+ { nombre: "Arroz con pollo", precio: 12, stock: 5 },
+ { nombre: "Lomo saltado", precio: 18, stock: 7 },
+ { nombre: "Sopa", precio: 8, stock: 10 },
+ { nombre: "chaufa", precio: 11, stock: 7 },
+ { nombre: "causa rellena", precio: 20, stock: 4 }
 ];
 
 function contarPlatos() {
@@ -44,7 +44,7 @@ function renderMenu() {
 
 // 3) FUNCIÓN: agregar un plato demo al menú
 function agregarPlatoDemo() {
-  const nuevoPlato = { nombre: "Pollo a la olla", precio: 19, stock: 6 };
+  const nuevoPlato = {  nombre: "cau cau", precio: 10, stock: 15  };
   const duplicado = menu.some(p =>
     p.nombre.toLowerCase() === nuevoPlato.nombre.toLowerCase()
   );
